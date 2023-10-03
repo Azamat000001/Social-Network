f889da92a47a9b21ef4787eec5f4761d0d1d4e2d
Azamat000001 <azamat.asatbekov@gmail.com>
2023-10-03 22:01:25 +0600

    Side Effect

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -5,18 +5,21 @@ import usersPhoto from "./../../assets/images/person-circle-icon.png"
 
 const Users = (props) => {
 
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items);
-            });
-
-        
+    let getUsers = () => {
+        if (props.users.length === 0) {
+                axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                    .then(response => {
+                        props.setUsers(response.data.items);
+                    });      
+        }
     }
+
+    
     
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
         {
                 props.users.map(u => <div key={u.id}>
                     <span>
